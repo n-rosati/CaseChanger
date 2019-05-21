5e8c222d187ad1e5b7787d0644f1832e4865dba7
Nicholas <nicholas@rosati.xyz>
2019-05-21 11:47:49 -0400

    Get the user's input option

--- main.c
+++ main.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
+#include <malloc.h>
+#include "functions.h"
 
 int main() {
+    char * userInput;
+    int userInputLength;
+
+    //Get valid user input
     do {
         printf("1. Read from console\n2. Read from file");
-    }while()
+        userInput = getField(malloc(sizeof(char)), stdin, &userInputLength);
+        if((userInputLength == 1) && ((int)userInput[0] == 49 || (int)userInput[0] == 50)){ break; }
+    }while(1);
 }
